b96f400dc24ad4d71e07fd2f7adf720ad87a377e
LucaFrioli <lucafrioli7@gmail.com>
2024-01-12 15:47:48 -0300

    Update Boilerplate
    creation of a boilerplate that will be used throughout the next modules

---
+++ zzZ_ArquivosUteis/boilerplate_main/src/main.js
@@ -0,0 +1,6 @@
+//webpack(auxiliador de transpilação de modo mais moderno) criando um boilerplate (um modelo onde se poderá fazer reuso)
+
+//lembre-se de caso necessário instalar as dependencias do projeto com : npm i
+// utilize o comando dentro do parenteses no terminal para iniciar o webpack: (npm run webpck)
+
+import './assets/css/style.css'//ele carrega o css na página 
